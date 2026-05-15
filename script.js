82591da166e218a2af8c0da0859bdcb2c0b01d61
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-15 11:08:36 -0300

    to do list parte 5

--- script.js
+++ script.js
@@ -4,19 +4,35 @@ function adicionar(){
         window.alert('Digite uma tarefa antes de adicionar!')
     }else{
         var telatasks = document.querySelector('.tela-task')
+        
         var tasks = document.createElement('div')
         tasks.setAttribute('class', 'tasks')
+        
         var txttask = document.createElement('div')
         txttask.setAttribute('class', 'texto-task')
-        var botaodel = document.createElement('input')
+        txttask.textContent = (txtinput.value)
+        
+        var botaodel = document.createElement('button')
         botaodel.setAttribute('class', 'botao-del')
+        var icone = document.createElement('i')
+        icone.setAttribute('class', 'fa-solid fa-xmark')
+
 
         telatasks.appendChild(tasks)
         
         tasks.appendChild(txttask)
         
         tasks.appendChild(botaodel)
+
+        botaodel.appendChild(icone)
+
+        txtinput.value = ''
         
-    
+        txtinput.focus()
     }
+}
+
+function excluir(){
+    var task = document.querySelector('.tasks')
+    task.remove('.task')
 }
